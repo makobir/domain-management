a86f55eb71b2d2d92e3ee71631d3d1ae54adf56b
Md Alamgir Kobir <makobirbd@gmail.com>
2025-05-12 21:44:05 +0600

    Update whoisapi.php

--- application/config/whoisapi.php
+++ application/config/whoisapi.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['whois_api_key'] = 'at_5jCWil66LhxlvChD5RUKT4eYhoIfu'; // Get this from whoisapi.whois.com
+$config['whois_api_key'] = 'YOUR-API-KEY'; // Get this from whoisapi.whois.com
 $config['whois_api_url'] = 'https://www.whoisxmlapi.com/whoisserver/WhoisService';
